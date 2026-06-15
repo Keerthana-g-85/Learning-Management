82470088c832a79f74bb9177015230944b3b6923
Keerthana G <keerthana@CELSYSBLR000337.local>
2026-06-15 23:30:22 +0530

    Database connection

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,6 @@
 import express  from 'express';
 import dotenv from 'dotenv';
+import { DataSource , type DataSourceOptions} from 'typeorm';
 
 dotenv.config()
 
@@ -7,7 +8,28 @@ const app = express()
 
 app.use(express.json())
 
+const data : DataSourceOptions = {
+    type : process.env.DB_TYPE as 'postgres',
+    host : process.env.DB_HOST as string ,
+    password : process.env.DB_PASSWORD as string ,
+    database : process.env.DB_NAME as string ,
+    username : process.env.DB_USER as string ,
+    port : Number(process.env.DB_PORT),
+}
+export const database = new DataSource(data)
 const port = process.env.PORT
+
+const connection = async () => {
+    try{
+    await database.initialize()
+    console.log("Database connected")
+    }
+    catch(error){
+        console.log("Unable to connect database ")
+    }
+}
+
+connection()
 
 app.listen(port , ()=>{
     console.log("server started")
