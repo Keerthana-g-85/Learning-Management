4deb77f0004d71b4ed27725ccc14f8c2735efd35
Keerthana G <keerthana@CELSYSBLR000337.local>
2026-06-23 15:10:02 +0530

    updating user

--- client/src/Dashboard/Users.tsx
+++ client/src/Dashboard/Users.tsx
@@ -10,6 +10,8 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
+import SaveIcon from '@mui/icons-material/Save'
+import { TextField } from "@mui/material";
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
@@ -32,6 +34,7 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
 }));
 
 interface Users{
+    id : string ,
     name : string,
     email : string ,
     address : string,
@@ -40,19 +43,28 @@ interface Users{
 }
 
 export default function User(){
-    const [instructor , setInstructor] = useState([])
+    const [user , setUser] = useState([])
+    const [edit , setEdit] = useState<Users | null>(null)
     useEffect(()=>{
         const getStudent = async()=>{
             try{
                 const response = await Api({method:'get' , endpoint:'/register/get'})
                 console.log(response)
-                setInstructor(response.data.user)
+                setUser(response.data.user)
             }catch(error){
                 console.log(error)
             }
         }
         getStudent()
     },[])
+    async function handleSave(){
+      if (edit === null) { return }
+      const response = await Api({method : 'put' ,endpoint :`/register/update/${edit?.id}` , data: edit })
+      console.log(response)
+      const getresponse = await Api({method:'get' , endpoint:'/register/get'})
+      setUser(getresponse.data.user)
+      setEdit(null)
+    }
     return(
         <>
         <TableContainer component={Paper}>
@@ -68,16 +80,54 @@ export default function User(){
           </TableRow>
         </TableHead>
         <TableBody>
-          {instructor.map((row: Users) => (
-            <StyledTableRow key={row.name}>
+          {user.map((row: Users) => (
+            <StyledTableRow key={row.id}>
+              
               <StyledTableCell component="th" scope="row">
-                {row.name}
+                {edit?.id === row?.id ?
+                <TextField size="small" id="standard-basic" variant="standard" 
+                value={edit.name} onChange={(e)=>setEdit({...edit, name:e.target.name})}/>:
+                row.name}
               </StyledTableCell>
-              <StyledTableCell align="right">{row.email}</StyledTableCell>
-              <StyledTableCell align="right">{row.phoneNumber }</StyledTableCell>
-              <StyledTableCell align="right">{row.address}</StyledTableCell>
-              <StyledTableCell align="right">{row.role}</StyledTableCell>
-              <StyledTableCell align="right"><EditIcon/><DeleteIcon/></StyledTableCell>
+
+              <StyledTableCell align="right">
+                {edit?.id === row.id ?
+                (<TextField size="small" id="standard-basic" variant="standard"
+                value={edit.email} onChange={(e) => setEdit({ ...edit, email: e.target.value, })}/>):
+                row.email}
+              </StyledTableCell>
+
+              <StyledTableCell align="right">
+                {edit?.id === row.id ? 
+                <TextField size="small" id="standard-basic" variant="standard" 
+                value={edit.phoneNumber} onChange={(e)=>setEdit({...edit, phoneNumber:e.target.value})}/>:
+                row.phoneNumber }
+              </StyledTableCell>
+
+              <StyledTableCell align="right">
+                {edit?.id === row?.id ?
+                <TextField size="small" id="standard-basic" variant="standard" 
+                value ={edit.address} onChange={(e)=>setEdit({...edit, address:e.target.value})}/>:
+                row.address}
+              </StyledTableCell>
+
+              <StyledTableCell align="right">
+                  {edit?.id === row?.id ? 
+                  <TextField size="small" id="standard-basic" variant="standard" 
+                  value={edit.role} onChange={(e)=>setEdit({...edit, role:e.target.value})} />:
+                  row.role}
+              </StyledTableCell>
+
+              <StyledTableCell align="right">
+                {
+                  edit?.id === row.id ? (
+                    <SaveIcon onClick={() => handleSave()} />
+                  ) : (
+                    <EditIcon onClick={() => setEdit(row)} />
+                  )
+                }
+                <DeleteIcon/>
+              </StyledTableCell>
             </StyledTableRow>
           ))}
         </TableBody>
